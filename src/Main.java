375e318429b9b40f5f0193719ab5273e977eba8b
Luis <luisrovirosa@gmail.com>
2013-05-20 10:03:49 +0200

    Desacoplando

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,33 @@ import com.scrum.Problem1;
 
 public class Main {
 
-	/**
-	 * @param args
-	 * @throws FileNotFoundException 
-	 */
-	public static void main(String[] args) throws FileNotFoundException {
-		System.out.println(args[0]);
-		File file = new File(args[0]);
+	public static String[] getResponse(String fileName)
+			throws FileNotFoundException {
+		File file = new File(fileName);
 		Scanner s = new Scanner(file);
 		int number = Integer.parseInt(s.nextLine());
+		String[] response = new String[number];
 		for (int i = 0; i < number; i++) {
 			String line = s.nextLine();
 			Problem1 p = new Problem1(line);
-			System.out.println(p.sort());
+			response[i] = p.sort();
+			// System.out.println(p.sort());
 		}
 		s.close();
+		return response;
+	}
 
+	/**
+	 * @param args
+	 * @throws FileNotFoundException
+	 */
+	public static void main(String[] args) throws FileNotFoundException {
+		System.out.println(args[0]);
+		String[] response = Main.getResponse(args[0]);
+
+		for (String lineResponse : response) {
+			System.out.println(lineResponse);
+		}
 	}
 
 }
