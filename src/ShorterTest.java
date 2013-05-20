10d6b8a445dc5ee4263830104d55f00668bb2b54
Luis <luisrovirosa@gmail.com>
2013-05-20 10:14:40 +0200

    Ya tenemos los test con el fichero de pruebas

--- src/ShorterTest.java
+++ src/ShorterTest.java
@@ -1,6 +1,8 @@
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 import org.junit.Test;
 
@@ -10,10 +12,11 @@ public class ShorterTest {
 
 	@Test
 	public void test() {
-		String[] inputs = {"2 b ba",  "6 facebook hacker cup for studious students",
+		String[] inputs = { "2 b ba",
+				"6 facebook hacker cup for studious students",
 				"5 k duz q rc lvraw", "5 mybea zdr yubx xe dyroiy",
 				"5 jibw ji jp bw jibw", "5 uiuy hopji li j dcyi" };
-		String[] responses = {"bab",  "cupfacebookforhackerstudentsstudious",
+		String[] responses = { "bab", "cupfacebookforhackerstudentsstudious",
 				"duzklvrawqrc", "dyroiymybeaxeyubxzdr", "bwjibwjibwjijp",
 				"dcyihopjijliuiuy"
 
@@ -21,13 +24,24 @@ public class ShorterTest {
 		for (int i = 0; i < inputs.length; i++) {
 			Problem1 problem = new Problem1(inputs[i]);
 			String solution = problem.sort();
-			assertEquals(responses[i],solution);
+			assertEquals(responses[i], solution);
 		}
 	}
-	
+
 	@Test
-	public void test2(){
-//		File inputFile = new File('file.txt');
+	public void test2() throws FileNotFoundException {
+		String[] response = Main
+				.getResponse("/home/zilc/workspace/Numeros/src/studious_student.in");
+
+		File f = new File("/home/zilc/workspace/Numeros/src/studious_student.out");
+		Scanner s = new Scanner(f);
+		int number = response.length;
+		for (int i = 0; i < number; i++) {
+			String line = s.nextLine();
+			assertEquals(line, response[i]);
+		}
 		
+		// File inputFile = new File('file.txt');
+
 	}
 }
